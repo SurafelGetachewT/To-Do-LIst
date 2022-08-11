cba0d23ccb3b4ce58437a8abce69532f82fc66b6
Surafel Getachew <suragetch@gmail.com>
2022-08-11 11:19:33 +0300

    Fix linter error

--- src/populate.js
+++ src/populate.js
@@ -1,12 +1,12 @@
 export const saveLocal = (list) => {
   window.localStorage.setItem('localTasks', JSON.stringify(list));
-}
+};
 
 export const add = (list) => {
   list.push({ description: document.querySelector('#newTask').value, isCompleted: false, index: list.length + 1 });
   document.querySelector('#newTask').value = '';
   saveLocal(list);
-}
+};
 
 export const updateIndex = (list) => {
   let i = 1;
@@ -14,4 +14,4 @@ export const updateIndex = (list) => {
     elem.index = i;
     i += 1;
   });
-}
+};
