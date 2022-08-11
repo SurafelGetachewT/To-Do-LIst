f4d587b365da30e76555c0f19d8b3837232b8f13
Surafel Getachew <suragetch@gmail.com>
2022-08-11 16:47:25 +0300

    Add status.js file for status update method

---
+++ src/status.js
@@ -0,0 +1,13 @@
+export const saveLocal = (list) => {
+  window.localStorage.setItem('localTasks', JSON.stringify(list));
+};
+
+
+export const status = (elem, list) => {
+  list.forEach((task) => {
+    if (task === elem) {
+      task.isCompleted = !task.isCompleted;
+    }
+  });
+  saveLocal(list);
+}
